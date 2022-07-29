749fb32e4b950de657102ec3b07117d295ef2a85
eunsang_lee <thecynicdog0328@gmail.com>
2022-07-29 11:14:52 +0900

    style

--- gamification/src/main/java/com/example/gamification/game/service/GameServiceImpl.java
+++ gamification/src/main/java/com/example/gamification/game/service/GameServiceImpl.java
@@ -26,35 +26,39 @@ class GameServiceImpl implements GameService {
 
     @Override
     public GameResult newAttemptForUser(final ChallengeSolvedDTO challenge) {
-        // We give points only if it's correct
+
         if (challenge.isCorrect()) {
-            ScoreCard scoreCard = new ScoreCard(challenge.getUserId(),
-                    challenge.getAttemptId());
+            ScoreCard scoreCard = new ScoreCard(challenge.getUserId(), challenge.getAttemptId());
             scoreRepository.save(scoreCard);
+
             log.info("User {} scored {} points for attempt id {}",
-                    challenge.getUserAlias(), scoreCard.getScore(),
-                    challenge.getAttemptId());
+                    challenge.getUserAlias(), scoreCard.getScore(), challenge.getAttemptId());
+
             List<BadgeCard> badgeCards = processForBadges(challenge);
-            return new GameResult(scoreCard.getScore(),
-                    badgeCards.stream().map(BadgeCard::getBadgeType)
+
+            return new GameResult(
+                    scoreCard.getScore(),
+                    badgeCards
+                            .stream()
+                            .map(BadgeCard::getBadgeType)
                             .collect(Collectors.toList()));
+
         } else {
-            log.info("Attempt id {} is not correct. " +
-                            "User {} does not get score.",
+            log.info("Attempt id {} is not correct. User {} does not get score.",
                     challenge.getAttemptId(),
                     challenge.getUserAlias());
             return new GameResult(0, List.of());
         }
     }
 
-    private List<BadgeCard> processForBadges(
-            final ChallengeSolvedDTO solvedChallenge) {
-        Optional<Integer> optTotalScore = scoreRepository.
-                getTotalScoreForUser(solvedChallenge.getUserId());
-        if (optTotalScore.isEmpty()) return Collections.emptyList();
+    private List<BadgeCard> processForBadges(final ChallengeSolvedDTO solvedChallenge) {
+
+        Optional<Integer> optTotalScore = scoreRepository
+                .getTotalScoreForUser(solvedChallenge.getUserId());
+        if (optTotalScore.isEmpty()) { return Collections.emptyList(); }
+
         int totalScore = optTotalScore.get();
 
-        // Gets the total score and existing badges for that user
         List<ScoreCard> scoreCardList = scoreRepository
                 .findByUserIdOrderByScoreTimestampDesc(solvedChallenge.getUserId());
         Set<BadgeType> alreadyGotBadges = badgeRepository
@@ -63,16 +67,11 @@ class GameServiceImpl implements GameService {
                 .map(BadgeCard::getBadgeType)
                 .collect(Collectors.toSet());
 
-        // Calls the badge processors for badges that the user doesn't have yet
         List<BadgeCard> newBadgeCards = badgeProcessors.stream()
-                .filter(bp -> !alreadyGotBadges.contains(bp.badgeType()))
-                .map(bp -> bp.processForOptionalBadge(totalScore,
-                        scoreCardList, solvedChallenge)
-                ).flatMap(Optional::stream) // returns an empty stream if empty
-                // maps the optionals if present to new BadgeCards
-                .map(badgeType ->
-                        new BadgeCard(solvedChallenge.getUserId(), badgeType)
-                )
+                .filter(badgeProcessor -> !alreadyGotBadges.contains(badgeProcessor.badgeType()))
+                .map(badgeProcessor -> badgeProcessor.processForOptionalBadge(totalScore, scoreCardList, solvedChallenge))
+                .flatMap(Optional::stream)
+                .map(badgeType -> new BadgeCard(solvedChallenge.getUserId(), badgeType))
                 .collect(Collectors.toList());
 
         badgeRepository.saveAll(newBadgeCards);

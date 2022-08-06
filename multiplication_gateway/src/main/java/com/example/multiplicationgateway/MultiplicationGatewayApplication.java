70d4f6a61696a0794baa571583588d5c4440a395
eunsang_lee <thecynicdog0328@gmail.com>
2022-08-06 19:39:21 +0900

    init

---
+++ multiplication_gateway/src/main/java/com/example/multiplicationgateway/MultiplicationGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiplicationgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplicationGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiplicationGatewayApplication.class, args);
+    }
+
+}
---
+++ multiplication_gateway/src/test/java/com/example/multiplicationgateway/MultiplicationGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiplicationgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplicationGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
